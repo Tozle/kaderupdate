78b09a8ce58187281cfb4b521f8694eee01f733a
Tozle <t.zeiler@outlook.de>
2025-08-22 23:18:57 +0200

    CSP: Setze Content-Security-Policy Header direkt in next.config.ts für Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+
+const ContentSecurityPolicy =
+  "default-src 'self'; " +
+  "script-src 'self' 'unsafe-inline' plausible.io; " +
+  "style-src 'self' 'unsafe-inline'; " +
+  "img-src * blob: data:; " +
+  "media-src 'none'; " +
+  "connect-src *; " +
+  "font-src 'self'; " +
+  "frame-src plausible.io;";
+
 const nextConfig: NextConfig = {
   // ...weitere config options hier...
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.ignoreWarnings = [
       {
